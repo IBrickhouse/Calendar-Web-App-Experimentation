9eeee33ef62d63b733ad955064a2f33cd2c01101
IBrickhouse <mrfurbyb@gmail.com>
2022-07-29 21:00:35 -0700

    Create Function To Add Events To List

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
+const today = new Date();
+
+const myEvents = document.querySelector('#myEvents')
 const who = document.querySelector('#who');
 const what = document.querySelector('#what');
 const when = document.querySelector('#when');
 const where = document.querySelector('#where');
 const why = document.querySelector('#why'); 
-const event = document.querySelector('.event');
-const monEventList = document.querySelector('#monEvents')
+const occasion = document.querySelector('.event');
+const monEventList = document.querySelector('#monthlyEvents');
 
-events.addEventlistener('submit', addSpecialEvent)
+myEvents.addEventListener('submit', addSpecialEvent);
 
-function addSpecialEvent(sub) {
-    sub.preventDefault();
+function addSpecialEvent(e) {
+    e.preventDefault();
 
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(`${who.value} : ${what.value} : ${when.value} : ${where.value} : ${why.value}`));
+    if(who.value=== '' || what.value === '' || when.value === '' || where.value === '' || why.value === '') {
+        occasion.classList.add('error');
+        occasion.innerHTML = 'Please Fill All Fields';
+    
+        setTimeout(() => occasion.remove(), 3000)
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${who.value} : ${what.value} : ${when.value} : ${where.value} : ${why.value}`));
 
-    monEventList.appendChild(li);
+        monEventList.appendChild(li);
 
-    who.value = '';
-    what.value = '';
-    when.value = '';
-    where.value = '';
-    why.value = '';
+        who.value = '';
+        what.value = '';
+        when.value = '';
+        where.value = '';
+        why.value = '';
+    }
 }
+
+function getTodaysDate() {
+    document.getElementById('dateDemo').innerHTML = today;
+
+    todaysNum = today.getDate();
+
+    console.log(todaysNum);
+}
+
+// getTodaysDate()
